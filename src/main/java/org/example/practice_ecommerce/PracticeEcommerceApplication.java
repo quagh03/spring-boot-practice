c2f3135a97787ad5a6051462b64cd1006e596269
quagh03 <03.quanghuy.le@gmail.com>
2024-04-14 08:48:57 +0700

    initial

---
+++ src/main/java/org/example/practice_ecommerce/PracticeEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.example.practice_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeEcommerceApplication.class, args);
+    }
+
+}
